fd87cc34aa8320c3e7e99749d9dd5aee8ae702b8
Swathi <swathi.h@perfios.com>
2021-09-08 21:09:02 +0530

    ADDED: Junit test for validating email for both true and false cases

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -13,7 +13,7 @@ public class UserRegistrationTest {
 	}
 	
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnFalse() {
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
 		UserRegistration userRegistration = new UserRegistration();
 		boolean isValid = userRegistration.validateName("swathi");
 		Assert.assertFalse(isValid);
@@ -27,9 +27,23 @@ public class UserRegistrationTest {
 	}
 	
 	@Test
-	public void givenLastName_WhenProper_ShouldReturnFalse() {
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
 		UserRegistration userRegistration = new UserRegistration();
 		boolean isValid = userRegistration.validateName("He");
+		Assert.assertFalse(isValid);
+	}
+	
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isValid = userRegistration.validateEmail("abc@gmail.com.co");
+		Assert.assertTrue(isValid);
+	}
+	
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isValid = userRegistration.validateEmail("abc@abc@gmail.com");
 		Assert.assertFalse(isValid);
 	}
 }
