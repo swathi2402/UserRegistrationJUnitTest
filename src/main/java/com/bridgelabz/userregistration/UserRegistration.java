d32966b77715788cd7c0a2eeed4c7ea313c99ddb
Swathi <swathi.h@perfios.com>
2021-09-08 21:04:50 +0530

    ADDED: Junit test for validating first name for both true and false

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public static boolean validateName(String name) {
+	public boolean validateName(String name) {
 		String regex = "([A-Z][a-zA-Z]{2,})";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(name);
@@ -13,7 +13,7 @@ public class UserRegistration {
 		
 	}
 	
-	public static boolean validateEmail(String email) {
+	public boolean validateEmail(String email) {
 		String regex = "([a-zA-Z][a-zA-Z0-9]*)(([+_.-][a-zA-Z0-9]+)?)(@[a-zA-Z0-9]+)([.])([a-z]{2,})(([.][a-z]{2})?)";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(email);
@@ -21,7 +21,7 @@ public class UserRegistration {
 		
 	}
 	
-	public static boolean validatePhoneNumber(String email) {
+	public boolean validatePhoneNumber(String email) {
 		String regex = "[1-9]{2}[\\s][1-9][0-9]{9}";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(email);
@@ -29,7 +29,7 @@ public class UserRegistration {
 		
 	}
 	
-	public static boolean validatePassword(String email) {
+	public boolean validatePassword(String email) {
 		String regex = "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*\\-+=]).{8,}";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(email);
---
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.userregistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+	
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isValid = userRegistration.validateName("Swathi");
+		Assert.assertTrue(isValid);
+	}
+	
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isValid = userRegistration.validateName("swathi");
+		Assert.assertFalse(isValid);
+	}
+
+}
