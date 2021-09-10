f789ad608c9128565e2af834246b0b18999ee596
Swathi <swathi.h@perfios.com>
2021-09-10 21:34:17 +0530

    Custom exception file to handle null and empty input

--- src/main/java/com/bridgelabz/userregistration/UserValidateException.java
+++ src/main/java/com/bridgelabz/userregistration/UserValidateException.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.userregistration;
 
-public class UserValidateException {
+public class UserValidateException extends Exception{
+	
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY
+	}
+	ExceptionType type;
 
+	public UserValidateException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
 }
